adb4f487c0eee62b5b94a68c7c215b2953268ef4
Taehyun <taehyunlee4482@gmail.com>
2018-11-17 02:07:21 -0500

    generate.py now adds random hex behind formats (generated once per format)

--- generate.py
+++ generate.py
@@ -3,6 +3,7 @@ import os
 import binascii
 from os import listdir, urandom
 from os.path import isfile, join, isdir, abspath
+import shutil
 
 # Parser Construction
 parser = argparse.ArgumentParser(
@@ -23,6 +24,7 @@ parser.add_argument(
 
 parser.add_argument(
   "-t", "--types",
+  default = "",
   nargs = "+",
   type = str,
   help = "File types to parse"
@@ -52,43 +54,56 @@ def get_random_hash():
 
 def grab_formats(file, formats):
   file_formats = {}
-  with open(file) as f:
+  temp = open("temp", "w")
+  with open(file, "r") as f:
     for line in f:
-      for word in line.split(" "):
+      line_for_temp = line.split(" ")
+
+      for ind, word in enumerate(line_for_temp):
         shouldAdd = False
+        format_word = ""
+        format_hash = ""
 
-        if(word.startswith("@!") 
-           and not(word[2:] in file_formats or word[2:-1] in file_formats)):
-          format_word = ""
-          if (word.endswith("\n")):
-            format_word = word[2:-1]
-          else:
-            format_word = word[2:]
-
-          # new format_word
-          if (not format_word in formats):
-            formats[format_word] = []
-
-          file_formats[format_word] = [get_random_hash(), []]
-          
-        elif(word.startswith("@?") 
-             and (word[2:] in file_formats or word[2:-1] in file_formats )):
-          format_word = ""
-          if (word.endswith("\n")):
+        if(word.startswith("@!") or word.startswith("@?")):
+          if (len(word.split("-")) > 1):
+            format_word = word.split("-")[0][2:]
+          elif (word.endswith("\n")):
             format_word = word[2:-1]
           else:
             format_word = word[2:]
           
-          formats[format_word].append(
-            file_formats.pop(format_word, None)
-          )
+          if(len(word.split("-")) == 1):
+            if(word.startswith("@!")):
+              format_hash = get_random_hash()
+            else:
+              format_hash = file_formats[format_word][0]
+            nl = "\n"
+            line_for_temp[ind] = f"{word[0:2]}{format_word}-{format_hash}{nl if word.endswith(nl) else ''}"
+          else:
+            format_hash = word.split("-")[1]
+
+          if(word.startswith("@!")):
+            # new format_word
+            if (not format_word in formats):
+              formats[format_word] = []
+
+            file_formats[format_word] = [format_hash, []]
+          else:
+            formats[format_word].append(
+              file_formats.pop(format_word, None)
+            )
         else:
           shouldAdd = True
         
         if (shouldAdd):
           for file_format in file_formats.values():
             file_format[1].append(word)
-  
+
+      temp.write(" ".join(line_for_temp))
+
+  temp.close()
+  shutil.move('temp', file)
+
   if (len(file_formats.keys())):
     print("The following format labels were not completed:")
     print(", ".join(file_formats.keys()))
---
+++ test.md
@@ -0,0 +1,12 @@
+@!Definition-feb44c7da15d9976 hey
+hi
+@?Definition-feb44c7da15d9976
+
+@!Definition-10c862386a169149
+hello
+@?Definition-10c862386a169149
+
+
+
+
+
